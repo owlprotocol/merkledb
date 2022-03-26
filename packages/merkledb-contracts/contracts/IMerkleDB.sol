8932b6c962f2e5ee3a315b1c844a87435d442fe9
HrikB <hrik.bhowal@gmail.com>
2022-03-25 23:36:36 -0700

    updat

--- packages/merkledb-contracts/contracts/IMerkleDB.sol
+++ packages/merkledb-contracts/contracts/IMerkleDB.sol
@@ -2,17 +2,12 @@
 pragma solidity ^0.8.0;
 
 interface IMerkleDB {
-    event MerkleUpdate(bytes32 merkleRoot, bytes32 merkleTreeIPFS);
-
-    event DatabaseUpdate(bytes32 databaseIPFS);
+    event MerkleUpdate(bytes32 merkleRoot);
 
     function merkleProofData(bytes calldata bytesLeaf, bytes32[] calldata path)
         external
         view
         returns (bool);
 
-    function updateMerkle(bytes32 _merkleRoot, bytes32 _merkleTreeIPFS)
-        external;
-
-    function updateDB(bytes32 _databaseIPFS) external;
+    function updateMerkle(bytes32 _merkleRoot) external;
 }
--- packages/merkledb-contracts/contracts/MerkleDB.sol
+++ packages/merkledb-contracts/contracts/MerkleDB.sol
@@ -7,8 +7,6 @@ import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProo
 
 contract MerkleDB is IMerkleDB, AccessControl {
     bytes32 public merkleRoot;
-    bytes32 public merkleTreeIPFS;
-    bytes32 public databaseIPFS;
 
     bytes32 public constant MERKLE_ROLE = keccak256("MERKLE_ROLE");
 
@@ -32,15 +30,7 @@ contract MerkleDB is IMerkleDB, AccessControl {
     {
         require(hasRole(MERKLE_ROLE, _msgSender()));
         merkleRoot = _merkleRoot;
-        merkleTreeIPFS = _merkleTreeIPFS;
 
-        emit MerkleUpdate(merkleRoot, merkleTreeIPFS);
-    }
-
-    function updateDB(bytes32 _databaseIPFS) external override {
-        require(hasRole(MERKLE_ROLE, _msgSender()));
-        databaseIPFS = _databaseIPFS;
-
-        emit DatabaseUpdate(databaseIPFS);
+        emit MerkleUpdate(merkleRoot);
     }
 }
