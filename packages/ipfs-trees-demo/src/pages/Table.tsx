3f9d458f8400d3cbfdd7c00c53768354535a9c4a
HrikB <hrik.bhowal@gmail.com>
2022-03-26 12:08:26 -0700

    table

--- packages/ipfs-trees-demo/src/pages/Table.tsx
+++ packages/ipfs-trees-demo/src/pages/Table.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './Table.css';
+
+const mockData = [
+    {
+        key: '97060505',
+        value: {
+            name: 'Bob',
+            score: 3,
+        },
+    },
+    {
+        key: '90695285',
+        value: {
+            name: 'Joe',
+            score: 17000,
+        },
+    },
+];
 
 function Table() {
-    return <div>Table</div>;
+    const { cid } = useParams();
+
+    const [json, setJson] = useState<any>(null);
+
+    useEffect(() => {
+        (async () => {
+            const data = await new Promise((res, rej) => setTimeout(() => res(mockData), 2000));
+            setJson(data);
+        })();
+    }, []);
+
+    return (
+        <div>
+            <strong>CID: {cid}</strong>
+            <table>
+                <tr>
+                    <th scope='col'>Key</th>
+                    <th scope='col'>Value</th>
+                </tr>
+                {json !== null &&
+                    json.map((d: any) => (
+                        <tr>
+                            <td>{d['key']}</td>
+                            <td>{JSON.stringify(d['value'])}</td>
+                        </tr>
+                    ))}
+            </table>
+        </div>
+    );
 }
 
 export default Table;
